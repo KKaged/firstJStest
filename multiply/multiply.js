e3fb979e1d4534c71f4859af573eabf10856eb12
KKaged <dhigaredaleon707@gmail.com>
2023-02-23 17:48:26 +0900

    Some changes and finished multiply.

---
+++ multiply/multiply.js
@@ -0,0 +1,4 @@
+const num1 = parseInt(prompt("We will now multiply two numbers! Enter a number" ));
+const num2 = parseInt(prompt("Enter the second number you would like to multiply!"));
+const multiply = num1 * num2;
+alert(multiply);
--- script.js
+++
@@ -1,4 +0,0 @@
-const sum = parseInt(prompt("What number shall we add today?"));
-const num1 = 7;
-const answer = sum + num1;
-alert(answer);
